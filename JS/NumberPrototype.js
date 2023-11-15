7f4daac92bdd0b5613e0917039d3d818219078c2
rnuravtsev <rnuravtsev@gmail.com>
2023-11-15 19:41:19 +0300

    proto + part of async

---
+++ JS/NumberPrototype.js
@@ -0,0 +1,9 @@
+Number.prototype.plus = function(value) {
+    return this + value;
+};
+
+Number.prototype.minus = function(value) {
+    return this - value;
+};
+
+console.log('911 | 🚑', (2).plus(3).minus(1)) // 4
